244ff13011e34154fff8f5df7517db2310eb8989
Pablo <paochoa@unirioja.es>
2021-03-07 18:36:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dialogforalert/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.dialogforalert;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickShowAlert(View view) {
+        AlertDialog.Builder myAlertBuilder = new AlertDialog.Builder(MainActivity.this);
+        myAlertBuilder.setTitle("Alert");
+        myAlertBuilder.setMessage("Click OK to continue, or Cancel to stop:");
+
+        //se crea una clase anónima para definir el método onClick del botón OK
+        myAlertBuilder.setPositiveButton("OK", new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked OK button.
+                        Toast.makeText(getApplicationContext(), "Pressed OK",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        //se crea una clase anónima para definir el método onClick del botón Cancel
+        myAlertBuilder.setNegativeButton("Cancel", new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User cancelled the dialog.
+                        Toast.makeText(getApplicationContext(), "Pressed Cancel",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        //Crea y muestra el dialogo de alerta
+        myAlertBuilder.show();
+    }
+}
